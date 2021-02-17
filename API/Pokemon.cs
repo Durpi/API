308962e1559d411d63e435d2b8e2334da7727051
Pontus <02pontuskarlsson@gmail.com>
2021-02-17 10:03:40 +0100

    Initial commit

---
+++ API/Pokemon.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace API
+{
+    public class Pokemon
+    {
+        public string name;
+        public int weight;
+        public int base_experience;
+    }
+}
---
+++ API/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            RestClient client = new RestClient("https://pokeapi.co/api/v2/");
+
+            RestRequest request = new RestRequest("pokemon/charmander");
+
+            IRestResponse response = client.Get(request);
+
+            //Console.WriteLine(response.Content);
+
+            Pokemon charmander = JsonConvert.DeserializeObject<Pokemon>(response.Content);
+
+            Console.WriteLine(charmander.base_experience);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
